87e42295b8d881d5ba1fed4cb70ead04f2d1bbba
colen0 <colen0@hjh1h20wjl.maritz.com>
2023-08-05 23:22:11 -0500

    updated script.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -75,10 +75,10 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
   }
   
   listPilot.innerHTML = `Pilot ${pilot} is ready to launch`;
-  listCopilot.innerHTML = `Co-pilot ${copilot} is ready to launch`;
+  listCopilot.innerHTML = `Co-pilot ${copilot} is ready for launch`;
 
   if (fuelLevel >= 10000 && cargoLevel <= 10000){
-    launchStatus.innerHTML = "Shuttle is ready for launch";
+    launchStatus.innerHTML = "Shuttle is Ready for launch";
     launchStatus.style.color = "rgb(65, 159, 106)";
     list.style.visibility = 'hidden';
   }
